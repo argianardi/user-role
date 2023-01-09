38b22bbcce30fd2bc4977d5adbe60796c3e54055
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-01-09 15:04:52 +0700

    add user controllers

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,3 +1,6 @@
-const controllers = {}; //assign controllers
+const users = require("./users"); //import users controllers
+const controllers = {}; //assign controllers (object of all controllers)
+
+controllers.users = users; //assign users controllers to controllers
 
 module.exports = controllers; //export controllers
