cac2979ed2b320b464fd62d6771a72a218032122
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-01-09 15:01:08 +0700

    add users router

--- src/index.js
+++ src/index.js
@@ -4,21 +4,20 @@ const compression = require("compression");
 const helmet = require("helmet");
 const cors = require("cors");
 require("dotenv").config();
+const usersRoutes = require("./routes/users");
 
 //initialize express
 const app = express();
 
 // use package
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cors());
 app.use(helmet());
 app.use(compression());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
-// Route
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// Routes
+app.use("/users", usersRoutes);
 
 // server listening
 const PORT = process.env.PORT || 6022;
