17113b557321ed3d24e702a955b06e68d52f1041
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-01-10 17:24:00 +0700

    add projects table model

--- src/configs/models/index.js
+++ src/configs/models/index.js
@@ -1,6 +1,12 @@
 const models = {}; //assign models
 const users = require("./users"); //import users
+//---------------------------------------------------------
+const projects = require("./projects"); //import projects
+//---------------------------------------------------------
 
 models.users = users; //assign users
+//---------------------------------------------------------
+models.projects = projects; //assign projects
+//---------------------------------------------------------
 
 module.exports = models; //export models
