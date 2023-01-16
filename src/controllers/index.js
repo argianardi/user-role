e8054a86cf4a1bd606c65cd721f73e8082ab78e5
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-01-16 14:51:03 +0700

    add auth controller

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,8 +1,10 @@
 const controllers = {}; //assign controllers (object of all controllers)
 const users = require("./users"); //import users controllers
 const projects = require("./projects"); //import projects controllers
+const auth = require("./auth");
 
 controllers.users = users; //assign users controllers to controllers
 controllers.projects = projects; //assign projects controllers to controllers
+controllers.auth = auth;
 
 module.exports = controllers; //export controllers
