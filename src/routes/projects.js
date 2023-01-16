04d00da15d1106cf5dc2e897e928bd77c4ebd5ed
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-01-16 14:48:22 +0700

    add auth middleware for projects routes

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -1,20 +1,27 @@
 const express = require("express"); //import express
 const router = express.Router(); //include express router
 const controllers = require("../controllers/index"); //import projects controllers
+const validateAuth = require("../middlewares/validateAuth");
+
+const auth = [validateAuth.isAuthenticated, validateAuth.isAdmin];
 
 // post request
-router.post("/", controllers.projects.post);
+router.post("/", auth, controllers.projects.post);
 
 // get request all data
-router.get("/", controllers.projects.getAll);
+router.get("/", validateAuth.isAuthenticated, controllers.projects.getAll);
 
 // get request one data by id
-router.get("/:id", controllers.projects.getOneById);
+router.get(
+  "/:id",
+  validateAuth.isAuthenticated,
+  controllers.projects.getOneById
+);
 
 //  put request
-router.put("/:id", controllers.projects.put);
+router.put("/:id", auth, controllers.projects.put);
 
 // delete request
-router.delete("/:id", controllers.projects.delete);
+router.delete("/:id", auth, controllers.projects.delete);
 
 module.exports = router;
